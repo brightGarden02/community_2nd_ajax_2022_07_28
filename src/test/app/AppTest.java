59119bfedfe20d4540b2282ee33942a0552317fe
rreone2017 <rreone2017@gmail.com>
2022-07-28 14:51:16 +0900

    get List<ArticleDto> type from arrayFormJson

--- src/test/app/AppTest.java
+++ src/test/app/AppTest.java
@@ -1,6 +1,7 @@
 package app;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.ll.exam.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
@@ -70,6 +71,22 @@ public class AppTest {
 
         assertThat(articleDtoOrigin).isEqualTo(articleDtoFromJson);
 
+    }
+
+
+    @Test
+    void ObjectMapper__jsonStrToArticleDtoList() {
+
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
+        articleDtos.add(new ArticleDto(2, "제목2", "내용2"));
+
+        String jsonStr = Ut.json.toStr(articleDtos, "");
+
+        List<ArticleDto> articleDtosFromJson = Ut.json.toObj(jsonStr, new TypeReference<>() {
+        }, null);
+
+        assertThat(articleDtosFromJson).isEqualTo(articleDtos);
     }
 
 
