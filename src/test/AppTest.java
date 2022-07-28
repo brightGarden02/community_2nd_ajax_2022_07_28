bcde8f807c763980c0a15f644463be197840e842
rreone2017 <rreone2017@gmail.com>
2022-07-28 11:31:20 +0900

    change object to json using jackson

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ll.exam.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,4 +12,17 @@ public class AppTest {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
     }
+
+    @Test
+    void 실험_ObjectMapper() throws JsonProcessingException {
+
+        ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+        assertThat(jsonStr).isNotBlank();
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim());
+    }
+
 }
